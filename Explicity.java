4ac4cd37018b7981ca153bb85d78ced18d40b6a9
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-10-22 22:40:27 +0530

    Add files via upload
    Type Casting

---
+++ Explicity.java
@@ -0,0 +1,7 @@
+public class Explicity {
+    public static void main(String[] args) {
+        double x=90.8;
+        int a=(int)x;
+        System.out.println(a);
+       }
+}
---
+++ Implicity.java
@@ -0,0 +1,8 @@
+//implicty type casting widning
+public class Implicity {
+    public static void main(String[] args) {
+        int a=230;
+        double b=a;
+        System.out.println(b);
+    }
+}

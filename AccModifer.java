ed858989386116fd8b9ea0b1d97f3c788f37f7a5
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-04 19:58:07 +0530

    Add files via upload

---
+++ AccModifer.java
@@ -0,0 +1,12 @@
+public class AccModifer {
+    private static int a=60;
+    static int b=90;
+     protected static int c=36;
+    public static int d=23;
+    
+public static void main(String[] args) {
+    System.out.println(AccModifer.a);
+    System.out.println(AccModifer.b);
+    System.out.println(AccModifer.c);
+    System.out.println(AccModifer.d);
+}}

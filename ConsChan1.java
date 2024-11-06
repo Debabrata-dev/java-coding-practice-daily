0e49413fed72b0fc9f6693607b6cfcad588c508e
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-06 23:18:36 +0530

    Add files via upload

---
+++ ConsChan1.java
@@ -0,0 +1,17 @@
+class pen{
+    pen(){
+        System.out.println("It has ink");
+    }
+    pen(int price){
+        this();
+        System.out.println("8");
+    }
+
+}
+
+public class ConsChan1 {
+    public static void main(String[] args) {
+        pen p=new pen(89);
+        
+    }
+}

9091d0226ea453c39d614750c32cf555c9ba11a9
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-08 22:43:48 +0530

    Add files via upload
    All Exception handeling

---
+++ Java Exceptions/ExceHandel.java
@@ -0,0 +1,13 @@
+public class ExceHandel
+{
+    public static void main(String[] args) {
+        int a=10,b=0,c;
+        try{
+        c=a/b;
+        
+        }
+        catch(ArithmeticException e){
+    System.out.println("Divide by zero");
+        }
+    }
+}

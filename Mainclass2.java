48d67fabb0b256cc76eafa01ee74aad067bdf7b1
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-14 00:08:08 +0530

    Add files via upload

--- Mainclass2.java
+++ Mainclass2.java
@@ -1,20 +1,19 @@
-package com.jspider.Access;
-
+class Car{
+int roll=90;
+}
+class Game extends Car{
+    String name="dets";
+    void dis(){
+        String name="Geses";
+        System.out.println(super.roll);
+        System.out.println(this.name);
+        System.out.println(name);
+    }
+}
 public class Mainclass2 {
-private String Empname;
-private int Empid;
-public String getEmpname() {
-	return Empname;
-}
-public void setEmpname(String empname) {
-	Empname = empname;
-}
-public int getEmpid() {
-	return Empid;
-}
-public void setEmpid(int empid) {
-	this.Empid=empid;
-}
+ public static void main(String[] args) {
+    Game obj=new Game();
+    obj.dis();
 
-
+    }
 }

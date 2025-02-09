d371d442e22a8a494afe6559775ecc047712a695
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2025-02-09 21:20:31 +0530

    Add files via upload

--- MainProgram.java
+++ MainProgram.java
@@ -1,22 +1,10 @@
-import java.util.Scanner;
+package Queue_Collection;
+
+import java.util.PriorityQueue;
 
 public class MainProgram {
-    public static void main(String[] args) {
-        int temp=0;
-        Scanner s1=new Scanner(System.in);
-        int num=s1.nextInt();
-        for(int i=1;i<=num;i++){
-            for(int j=2;j<=i-1;j++){
-                if(i%j==0){
-                    temp++;
-                }
-            }
-            if(temp==0){
-                System.out.println(i);
-            }
-            else{
-                temp=0;
-            }
-        }
-    }
+public static void main(String[] args) {
+	PriorityQueue p1=new PriorityQueue();
+	
+}
 }
